70391b5ff3e1a2ebaec12acd11a8357f78ade662
Tyler Hutchinson <jhutchinson192390@mgmail.com>
2022-09-05 23:51:58 -0400

    added my top five

--- src/component/MajorProject.js
+++ src/component/MajorProject.js
@@ -28,12 +28,21 @@ export default function MajorProject(props) {
                   Thanks for stopping by I'm James, an inspiring web developer
                   with an associates in computer networking, and a bechelors of
                   Business. After graduating I spent two years working in retail
-                  management.
+                  management. Now I'm living my dream of Programing.
                 </p>
                 <p>
                   In my spare time I enjoy spending time with my beautiful new
-                  baby girl and wife. 
+                  baby girl and wife. Avid collector of video games, vinyl
+                  records, and rare books. 
                 </p>
+                <h4> Top 5</h4>
+                <ul>
+                  <li>Authentic Ramen</li>
+                  <li>Any FromSoftware title</li>
+                  <li>Nausicaa of the valley of the wind (manga) by Hayao Miyazaki</li>
+                  <li>Tolken</li>
+                  <li>All the other stuff</li>
+                </ul>
               </div>
             </div>
             <div className="socials-project">Socials</div>
