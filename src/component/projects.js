25c055fc710f638dd3a4fcc210d34f943fa17170
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-09-07 00:18:13 -0400

    handled PROBLEM:
    eslint picking up uneeded dependices

--- src/component/projects.js
+++ src/component/projects.js
@@ -48,6 +48,7 @@ export default function Projects(props) {
       setSelectedPost( ...sanity.filter((post) => post._createdAt === urlParams));
       console.log(selectedPost);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [sanity]);
 
   return (
