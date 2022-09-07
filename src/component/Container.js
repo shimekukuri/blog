f8e8b366661612140402b443a4dfdd4ff8fa3f62
Tyler Hutchinson <jhutchinson192390@gmail.com>
2022-09-06 23:55:18 -0400

    removed useState

--- src/component/Container.js
+++ src/component/Container.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./Container.css";
 import clsx from "clsx";
 import { useInView } from "react-intersection-observer";
